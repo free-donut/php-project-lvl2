4d964459daf733a4a49fd3699976c038b2c09865
free-donut <45130739+free-donut@users.noreply.github.com>
2019-07-31 15:14:57 +0300

    Update diffTest.php

--- tests/diffTest.php
+++ tests/diffTest.php
@@ -10,7 +10,7 @@ class diffTest extends TestCase
 {
     public function testGetDiff()
     {
-    	$equals = "{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: 1\n}\n";
+    	$equals = "{\n    host: hexlet.io\n  + timeout: 20\n  - timeout: 50\n  - proxy: 123.234.53.22\n  + verbose: true\n}\n";
 
         $actual = getDiff('tests/testFiles/before.json', 'tests/testFiles/after.json');
 
