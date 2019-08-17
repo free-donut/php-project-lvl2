ae7444752f035ab975bdb43285bf5d4d569e1ab4
free-donut <yula@freedonut.space>
2019-08-17 16:22:44 +0300

    refactoring

--- src/builder.php
+++ src/builder.php
@@ -28,34 +28,29 @@ function buildAST($array1, $array2)
       	//вернуть АСД массивов
       	$child = buildAST($array1[$key], $array2[$key]);
       	$elem = buildNode('array', $key, null, null, $child);
-      	//$elem = AST($array1[$key], $array2[$key]);
       	$acc[] = $elem; 
         return $acc;
         // если оба значения равны
       } if ($array1[$key] == $array2[$key]) {
       	//вернуть АСД с типом "без изменений"
       	$elem = buildNode('unchanged', $key, $array1[$key], $array2[$key], '');
-        $acc[] = $elem;
-        return $acc;
       } else {
       	//венуть АСД с типом "изменен"
       	$elem = buildNode('changed', $key, $array1[$key], $array2[$key], '');
-        $acc[] = $elem;
-        return $acc;
       }
+      $acc[] = $elem;
+      return $acc;    
     // если существует только первое значение
     } if (isset($array1[$key])) {
       //вернуть АСД с типом "удален"
       $elem = buildNode('deleted', $key, $array1[$key], null, '');
-      $acc[] = $elem;
-      return $acc;
     //если существует только второе значение
     } else {
       //вернуть АСД со значением "добавен"
       $elem = buildNode('added', $key, null, $array2[$key], '');
-      $acc[] = $elem;
-      return $acc;
     }
+      $acc[] = $elem;
+      return $acc;    
   }, []);
   
   return $reduceArray;
--- src/render.php
+++ src/render.php
@@ -27,29 +27,23 @@ function arrayToString($array, $indent = '')
 
 function render($ast, $indent = '')
 {
-
   $reduceArray = array_reduce($ast, function ($acc, $node) use ($indent) {
     if ($node["type"] === "array") {
-      # code...
       $newIndent = $indent . INDENT;
       $key = $node['key'];
       $nested = render($node['child'], $newIndent);
       $newAcc = $acc . INDENT . $key .": {\n" . $nested .INDENT . "}\n";
       return $newAcc;
     } if ($node["type"] === "unchanged") {
-      # code...
       $key = $node['key'];     
       if (is_array($node['beforeValue'])) {
-        # code...
         $value = arrayToString($node['beforeValue']);
-        $newAcc = $acc . $indent . INDENT .  $key . ": $value\n";
-        return $newAcc;
+      } else {
+        $value = boolToString($node['beforeValue']);
       }
-      $value = boolToString($node['beforeValue']);
       $newAcc = $acc . $indent . INDENT . $key . ": $value\n";
       return $newAcc;
     } if ($node['type'] == 'changed') {
-      # code...
       $key = $node['key'];
       $beforeValue = boolToString($node['beforeValue']);
       $afterValue = boolToString($node['afterValue']);
@@ -60,20 +54,20 @@ function render($ast, $indent = '')
       if (is_array($node['beforeValue'])) {
         $deletedElem = arrayToString($node['beforeValue'], INDENT);
         $newAcc = $acc . $indent . DELETE. $key . ": {\n" . $indent . INDENT. $deletedElem . $indent . INDENT . "}\n";
-        return $newAcc;
+      } else {
+        $deletedElem = boolToString($node['beforeValue']);
+        $newAcc = $acc . $indent . DELETE . $key . ": $deletedElem\n";        
       }
-      $deletedElem = boolToString($node['beforeValue']);
-      $newAcc = $acc . $indent . DELETE . $key . ": $deletedElem\n";
       return $newAcc;
     } if ($node['type'] == 'added') {
       $key = $node['key'];
       if (is_array($node['afterValue'])) {
         $addedElem = arrayToString($node['afterValue'], INDENT);
         $newAcc = $acc . $indent . ADD . $key . ": {\n" . $indent . INDENT . $addedElem . $indent . INDENT . "}\n";
-        return $newAcc;
+      } else {
+        $addedElem = boolToString($node['afterValue']);
+        $newAcc = $acc . $indent . ADD . $key . ": $addedElem\n";        
       }
-      $addedElem = boolToString($node['afterValue']);
-      $newAcc = $acc . $indent . ADD . $key . ": $addedElem\n";
       return $newAcc;
     }
   }, '');
