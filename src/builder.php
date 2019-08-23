dc38c1c5cc62b4ae9b73eb7a0959755867d98326
free-donut <yula@freedonut.space>
2019-08-23 18:39:18 +0300

    refactoring

--- src/builder.php
+++ src/builder.php
@@ -16,45 +16,6 @@ function buildNode($type, $key, $beforeValue, $afterValue, $child = null)
   return $node;
 }
 
-function buildAST($beforeData, $afterData)
-{
-  $unionKeys = Collection\union(array_keys($beforeData), array_keys($afterData));
-
-  $ast = array_reduce($unionKeys, function ($acc, $key) use ($beforeData, $afterData) {
-  	//если существуют оба значения
-    if (isset($beforeData[$key]) && isset($afterData[$key])) {
-      if ($beforeData[$key] == $afterData[$key]) {
-        //вернуть АСД с типом "без изменений"
-        $elem = buildNode('unchanged', $key, $beforeData[$key], $afterData[$key], '');
-      //если оба значения - массивы
-        $acc[] = $elem;
-        return $acc;         
-      } if (is_array($beforeData[$key]) && is_array($afterData[$key])) {
-        $child = buildAST($beforeData[$key], $afterData[$key]);
-        $elem = buildNode('array', $key, null, null, $child);
-      } else {
-      	//венуть АСД с типом "изменен"
-      	$elem = buildNode('changed', $key, $beforeData[$key], $afterData[$key], '');
-      }
-      $acc[] = $elem;
-      return $acc;    
-    // если существует только первое значение
-    } if (isset($beforeData[$key])) {
-      //вернуть АСД с типом "удален"
-      $elem = buildNode('deleted', $key, $beforeData[$key], null, '');
-    //если существует только второе значение
-    } else {
-      //вернуть АСД со значением "добавен"
-      $elem = buildNode('added', $key, null, $afterData[$key], '');
-    }
-      $acc[] = $elem;
-      return $acc;    
-  }, []);
-  
-  return $ast;
-}
-
-
 function buildMapAST($beforeData, $afterData)
 {
   $unionKeys = Collection\union(array_keys($beforeData), array_keys($afterData));
@@ -65,7 +26,7 @@ function buildMapAST($beforeData, $afterData)
       if ($beforeData[$key] == $afterData[$key]) {
         //вернуть АСД с типом "без изменений"
         $node = buildNode('unchanged', $key, $beforeData[$key], $afterData[$key], '');
-        return $elem;     
+        return $node;     
       //если оба значения - массивы
       } if (is_array($beforeData[$key]) && is_array($afterData[$key])) {
         //вернуть вложенный node
