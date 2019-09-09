e94847dbc13f8f60d2fcdb2a259c38c33d93f974
free-donut <yula@freedonut.space>
2019-09-09 22:03:43 +0300

    fix tests

--- tests/diffTest.php
+++ tests/diffTest.php
@@ -5,11 +5,6 @@ namespace GenDiff\Tests;
 use PHPUnit\Framework\TestCase;
 
 use function GenDiff\Diff\getDiff;
-use function GenDiff\Parse\parser;
-use function GenDiff\Builder\buildAST;
-use function GenDiff\Render\getRender;
-use function GenDiff\Plain\getPlain;
-
 
 class DiffTest extends TestCase
 {
@@ -18,8 +13,8 @@ class DiffTest extends TestCase
 
     public function setUp() : void
     {
-        $this->pathToExpectedFile = __DIR__ . '/fixtures/';
-        $this->pathToTestFile = __DIR__ . '/testFiles/';
+        $this->pathToExpectedFile = __DIR__ . '/fixtures/expected/';
+        $this->pathToTestedFile = __DIR__ . '/fixtures/testable/';
     }
 
     /**
@@ -29,8 +24,8 @@ class DiffTest extends TestCase
     public function testGetDiff($expectedFile, $beforeFile, $afterFile, $format)
     {
         $expected = file_get_contents($this->pathToExpectedFile . $expectedFile);
-        $pathToBeforeFile = $this->pathToTestFile . $beforeFile;
-        $pathToAfterFile = $this->pathToTestFile . $afterFile;
+        $pathToBeforeFile = $this->pathToTestedFile . $beforeFile;
+        $pathToAfterFile = $this->pathToTestedFile . $afterFile;
         $actual = getDiff($pathToBeforeFile, $pathToAfterFile, $format);
         $this->assertEquals($expected, $actual);
     }
@@ -42,101 +37,6 @@ class DiffTest extends TestCase
             ['json_nested', 'before1.json', 'after1.json', 'json'],
             ['plain_flat', 'before.yml', 'after.yml', 'plain'],
             ['plain_nested', 'before1.json', 'after1.json', 'plain']
-        ];
-    }
-
-    /**
-    * @dataProvider additionProviderParser
-    */
-
-    public function testParser($expected, $file)
-    {
-        $pathToFile = $this->pathToTestFile . $file;
-        $actual = parser($pathToFile);
-        $this->assertEquals($expected, $actual);
-    }
-
-    public function additionProviderParser()
-    {
-        $out = [];
-
-        $out[] =  [
-            ["host" => "hexlet.io", "timeout" => 50, "proxy" => "123.234.53.22"],
-            'before.json'
-        ];
-
-        $out[] =  [
-            ["host" => "hexlet.io", "timeout" => 50, "proxy" => "123.234.53.22"],
-            'before.yml'
-        ];
-
-        return $out;
-    }
-
-    /**
-    * @dataProvider additionProviderBuilder
-    */
-
-    public function testBuildAST($expected, $beforeFile, $afterFile)
-    {
-        
-        $expected = require($this->pathToExpectedFile . $expected);
-        $beforeData = parser($this->pathToTestFile . $beforeFile);
-        $afterData = parser($this->pathToTestFile . $afterFile);
-        $actual = buildAST($beforeData, $afterData);
-        $this->assertEquals($expected, $actual);
-    }
-
-    public function additionProviderBuilder()
-    {
-        return [
-            ['expected_ast.php', 'before.json', 'after.json'],
-            ['expected_ast.php', 'before.yml', 'after.yml']
-        ];
-    }
-
-    /**
-    * @dataProvider additionProviderRender
-    */
-
-    public function testGetRender($expectedFile, $beforeFile, $afterFile)
-    {
-        $expected = file_get_contents($this->pathToExpectedFile . $expectedFile);
-        $beforeData = \GenDiff\Parse\parser($this->pathToTestFile . $beforeFile);
-        $afterData = \GenDiff\Parse\parser($this->pathToTestFile . $afterFile);
-        $ast = \GenDiff\Builder\buildAST($beforeData, $afterData);
-        $actual = getRender($ast, '');
-        $this->assertEquals($expected, $actual);
-    }
-
-    public function additionProviderRender()
-    {
-        return [
-            ['json_flat', 'before.json', 'after.json'],
-            ['json_flat', 'before.yml', 'after.yml'],
-            ['json_nested', 'before1.json', 'after1.json']
-        ];
-    }
-
-    /**
-    * @dataProvider additionProviderPlain
-    */
-
-    public function testGetPlain($expectedFile, $beforeFile, $afterFile)
-    {
-        $expected = file_get_contents($this->pathToExpectedFile . $expectedFile);
-        $beforeData = \GenDiff\Parse\parser($this->pathToTestFile . $beforeFile);
-        $afterData = \GenDiff\Parse\parser($this->pathToTestFile . $afterFile);
-        $ast = \GenDiff\Builder\buildAST($beforeData, $afterData);
-        $actual = getPlain($ast);
-        $this->assertEquals($expected, $actual);
-    }
-
-    public function additionProviderPlain()
-    {
-        return [
-            ['plain_flat', 'before.json', 'after.json'],
-            ['plain_nested', 'before1.json', 'after1.json']
         ];
     }
 }
